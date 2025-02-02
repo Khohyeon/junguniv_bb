9a2e9072d4400b775f75b1ce43d07546f30cbbfc
Merhong <zx2938@gmail.com>
2025-02-02 20:59:20 +0900

    기본표시정보 - 환급교육

---
+++ src/main/java/com/example/junguniv_bb/system/settings/controller/MarkNormalRestController.java
@@ -0,0 +1,14 @@
+package com.example.junguniv_bb.system.settings.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/masterpage_sys/mark/normal/api")
+@RequiredArgsConstructor
+@Slf4j
+public class MarkNormalRestController {
+}
